3a58b8ee27f8260141e23c52fac477a1efe43a91
Sam Taylor <2015samtaylor@gmail.com>
2024-02-12 17:28:11 -0800

    email_scrape_main is production ready

--- SQL_Scripts/email_failures.py
+++ SQL_Scripts/email_failures.py
@@ -5,6 +5,44 @@ import pymysql
 from pymysql import Error
 from config import ssh_username, ssh_host, ssh_password, ssh_port, mysql_database, mysql_host, mysql_password, mysql_port, mysql_username
 from datetime import datetime
+import logging
+
+
+def find_bad_emails(outbox, failures, subject_line):
+        
+        try:
+            #This merge occurs in order to link the message_id on the failures, ultimately finding the emailaddress
+            failures = failures[['message_id', 'references']]
+            failures = failures.rename(columns = {'message_id': 'message_id_failed', 'references': 'references_failed'})
+
+            #When outbox is empty here, failures is still present
+
+            failures = pd.merge(outbox, failures, left_on='message_id', right_on='references_failed')   #merge the reference id on the initial outbound message
+          
+            failures = failures.rename(columns = {'to': 'emailaddress'})
+
+            #Maybe write directly to the customer table
+            failures = failures[['emailaddress']].drop_duplicates().reset_index(drop = True)
+    
+            # Get current date and time
+            current_datetime = datetime.now()
+            formatted_date = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
+            failures['created_at'] = formatted_date
+            # Convert the 'created_at' column to datetime format if it's not already
+            failures['created_at'] = pd.to_datetime(failures['created_at'])
+    
+            print(f'Updating bad emails table with {len(failures)} emails - subject line {subject_line}')
+            logging.info(f'Updating bad emails table with {len(failures)} emails - subject line {subject_line}')
+            return(failures)
+        
+        except:
+            print(f'No bad emails to upate with {subject_line}')
+            failures = pd.DataFrame()
+            return(failures)
+
+
+        
+
 
 
 def update_bad_emails(df):
@@ -39,14 +77,15 @@ def update_bad_emails(df):
                         if e.args[0] == 1062:
                             print(f"Email {row['emailaddress']} already exists")
                         else:
-                            print(f'An error occured: {e}')
+                            print(f'First except block An error occured: {e}')
 
             # Commit the changes
             conn.commit()
             print('All new emails sent updated succesfully')
         
         except Exception as e:
-            print('An error occured: {e}')
+            print(f'Second except block An error occured: {e}')
+            print(type(e)) 
 
         finally:
             conn.close()
--- update_bad_emails.py
+++ update_bad_emails.py
@@ -15,8 +15,7 @@ def gather_and_send_bad_emails_to_db(subject_line, email_address, email_pass):
 
     current_date = datetime.now()
     previous_date = current_date - timedelta(days=1)
-    # formatted_date = previous_date.strftime('%m/%d/%Y')
-    formatted_date = '11/01/2023'
+    formatted_date = previous_date.strftime('%m/%d/%Y')
     #The reason the date is up today is because the faulty emails from the past are already in the DB
 
 
@@ -29,30 +28,13 @@ def gather_and_send_bad_emails_to_db(subject_line, email_address, email_pass):
     failures = scrape.scrape_msgs_outbox_or_inbox('inbox', 'Delivery Status Notification', email_address, email_pass, formatted_date)
     failures = scrape.create_msg_frame(failures)
 
+    #givne the fact that its iterating through subject lines there will always be something for outbox or failures
     failures = find_bad_emails(outbox, failures, subject_line)
-    
-    #failures will return as None if except block is hit
-    if failures is not None:
-        print(len(failures))
+
+    if failures.empty != True:
         #the merge is returning nothing here causing update to occur anyways. Debug this portion and figure out the logic
         update_bad_emails(failures)
     
     else:
         print('No emails to update')
         logging.info('No bad emails to update')
-
-
-email_address = 'team@customplanet.com'
-email_pass = imap_password_customplanet
-server = 'emailcampaign.c9vhoi6ncot7.us-east-1.rds.amazonaws.com'
-database = 'emailcampaign'
-db = 'emailcampaign'
-
-db_connector_email_history = DatabaseConnector(server, database, db_username, db_password, db, 'email_history')
-query = db_connector_email_history.SQL_query('SELECT DISTINCT subject FROM [emailcampaign].[dbo].[email_history]')
-
-
-#Iterate through all unique subject lines to update the db table with all faulty email addresses
-for line in query['subject']:
-    print(line)
-    gather_and_send_bad_emails_to_db(line, email_address, email_pass)
